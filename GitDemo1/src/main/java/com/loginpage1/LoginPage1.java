2ab4000bca370c4aeaa7b4c867e43eb68b69e627
tushar nagargoje <tushar138336@gmail.com>
2023-04-21 12:10:51 +0530

    First commit

---
+++ GitDemo1/src/main/java/com/loginpage1/LoginPage1.java
@@ -0,0 +1,14 @@
+package com.loginpage1;
+
+public class LoginPage1 {
+	
+	public void loginTest() {
+		System.out.println("this is login test");
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
