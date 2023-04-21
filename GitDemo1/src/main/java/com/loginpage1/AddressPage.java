a1913cac3447b842a60cd59721c25a2c0c69fde6
tushar nagargoje <tushar138336@gmail.com>
2023-04-21 13:18:03 +0530

    worked on address test

---
+++ GitDemo1/src/main/java/com/loginpage1/AddressPage.java
@@ -0,0 +1,14 @@
+package com.loginpage1;
+
+public class AddressPage {
+	
+	public void addressTest() {
+		System.out.println("address test");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
