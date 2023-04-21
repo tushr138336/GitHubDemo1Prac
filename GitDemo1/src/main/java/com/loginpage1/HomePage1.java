d562feed65b7c9c0ca6310da24006adb6dc7909d
tushar nagargoje <tushar138336@gmail.com>
2023-04-21 13:07:19 +0530

    worked on home page

---
+++ GitDemo1/src/main/java/com/loginpage1/HomePage1.java
@@ -0,0 +1,15 @@
+package com.loginpage1;
+
+public class HomePage1 {
+	
+	
+	public void homePage() {
+		System.out.println("this is home page test");
+	}
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
