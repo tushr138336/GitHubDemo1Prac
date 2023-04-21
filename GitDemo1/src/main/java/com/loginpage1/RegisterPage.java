d513704a1ce98002b3a22ddb0cb6f6657cf1498c
tushar nagargoje <tushar138336@gmail.com>
2023-04-21 13:14:24 +0530

    worked on register test

---
+++ GitDemo1/src/main/java/com/loginpage1/RegisterPage.java
@@ -0,0 +1,14 @@
+package com.loginpage1;
+
+public class RegisterPage {
+	
+	public void registerTest() {
+		System.out.println("register test");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
